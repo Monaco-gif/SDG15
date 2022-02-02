deea67f5945961c3adb3af5f40ddbbf6ee666ba9
Monaco-gif <monaco.matteo@iisgalvanimi.edu.it>
2022-02-02 12:56:37 +0000

    da sistemare

--- client-angular/src/app/forest/forest.component.ts
+++ client-angular/src/app/forest/forest.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { ChartData } from '../models/chart.model';
 import { ForestArea } from '../models/forest_area.model';
 
 @Component({
@@ -10,6 +11,8 @@ import { ForestArea } from '../models/forest_area.model';
 })
 export class ForestComponent implements OnInit {
   obsforestarea:Observable<ForestArea[]>
+  chartData = [];
+  chartDataArray = [];
 
   constructor(public http: HttpClient) { 
 
@@ -17,21 +20,16 @@ export class ForestComponent implements OnInit {
   forestareaData = (data: ForestArea[]) => {
    
     console.log(data);
-    /*for (var i in data) {
+    for (var i in data) {
       this.chartData.push([
         data[i]['Entity'],
-        Math.log(parseInt(String(data[i]['EN_MAR_BEALITSQ']))),
-        `
-        <div>
-        <h4 style="color: #6F016A;">${parseInt(
-          String(data[i]['EN_MAR_BEALITSQ'])
-        ).toLocaleString('de-DE')} plastic items per square km </h4>
-        <p style="margin-top: -12px">${String(data[i]['Year'])}</p>
-        </div>
-        `,
+        data[i]['Forest_cover'],
       ]);
-    }*/
+    }
+    this.chartDataArray = [];
+    this.chartDataArray.push(new ChartData('EN_MAR_BEALITSQ', 'GeoChart', this.chartData, ['Entity', 'Forest_cover'], {}));
   }
+
   ngOnInit(): void {
     this.obsforestarea = this.http.get<ForestArea[]>(
       'https://5000-monacogif-sdg15-t5tx8vknusp.ws-eu30.gitpod.io/forest_area'
